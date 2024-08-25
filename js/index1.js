7a8f92f9e9e528457d1f4d0a5294f8429b9f13a3
cuongismeVN <cskh.cuongisme@gmail.com>
2024-08-25 22:28:38 +0700

    index1.js

--- js/index1.js
+++ js/index1.js
@@ -1,120 +1,111 @@
-particlesJS('particles-js',
 
-    {
-        "particles": {
-            "number": {
-                "value": 80,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#ffffff"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                },
-                "polygon": {
-                    "nb_sides": 5
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 5,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 150,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 6,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": true,
-        "config_demo": {
-            "hide_card": false,
-            "background_color": "#b61924",
-            "background_image": "",
-            "background_position": "50% 50%",
-            "background_repeat": "no-repeat",
-            "background_size": "cover"
-        }
-    }
+						var app = new Vue({
+							el: "#app",
+							data() {
+								return {
+									ifshow: true,
+									checked: false,
+									radio: '1',
+									activeSelect: '',
+									selectOption: [{
 
-);
+									}],
+									input: "",
+									showOption: false,
+									tabValue: "one",
+								}
+							},
+							mounted() {
+								this.setRect(360, 320);
+
+								//设置不可拖动
+								setWindowDrag(0, 0, 0, 0);
+
+								var layout = function () {
+									//window.orientation是设备握持方向, 不是屏幕显示方向
+									if (window.lastorientation == window.orientation) return;
+									window.lastorientation = window.orientation;
+
+									//window.screen中的宽高不会随着屏幕旋转更新(只会在初始化的时候固定)
+									if (Math.abs(window.orientation) == 90) {
+										//横屏模式
+										setWindowRect(0, 0, window.screen.height, window.screen.width);
+									} else {
+										//竖屏模式
+										setWindowRect(0, 0, window.screen.width, window.screen.height);
+									}
+								}
+
+								layout(); //设置旋转屏幕时自动调整布局和画布
+								window.addEventListener("orientationchange", layout, false);
+
+								setButtonAction(function () {
+									var menu = document.querySelector("#app");
+									if (menu.style.display == 'none') {
+										menu.style.display = 'block';
+										//显示菜单之后, 设置触控不可穿透悬浮窗口
+										setWindowTouch(true);
+									} else {
+										menu.style.display = 'none';
+										//隐藏菜单之后, 设置触控穿透悬浮窗口
+										setWindowTouch(false);
+									}
+								});
+							},
+
+							methods: {
+								setRect(w, h, x = -1, y = -1) {
+									var boxW = w;
+									var boxH = h;
+
+									var ayMenu = this.$refs.menuMain;
+									ayMenu.style.width = `${boxW}px`;
+									ayMenu.style.height = `${boxH}px`;
+									if (x == -1) ayMenu.style.left = `calc(50% - ${boxW / 2}px)`;
+									if (y == -1) ayMenu.style.top = `calc(50% - ${boxH / 2}px)`;
+
+									ayMenu.style.width = "400px";
+									ayMenu.style.height = "240px";
+
+
+
+
+									ayMenu.style.borderBottomLeftRadius = "10px";
+									ayMenu.style.borderBottomRightRadius = "10px";
+									ayMenu.style.borderTopLeftRadius = "10px";
+									ayMenu.style.borderTopRightRadius = "10px";
+								},
+								titleTouchStart(event) {
+
+									this.touchStartX = parseInt(event.touches[0].clientX);
+									this.touchStartY = parseInt(event.touches[0].clientY);
+
+									var ayMenu = this.$refs.menuMain;
+									this.menuLastX = ayMenu.offsetLeft;
+									this.menuLastY = ayMenu.offsetTop;
+									document.ayimgui.style.height = '1px';
+									document.container.style.height = '1px';
+									document.aybody.style.height = '1px';
+									document.aytab.style.height = '1px';
+
+								},
+								titleTouchMove(event) {
+									event.preventDefault();
+									var distanceX = event.touches[0].clientX - this.touchStartX;
+									var distanceY = event.touches[0].clientY - this.touchStartY;
+
+									var ayMenu = this.$refs.menuMain;
+									ayMenu.style.left = this.menuLastX + distanceX + "px";
+									ayMenu.style.top = this.menuLastY + distanceY + "px";
+								},
+								//切换导航栏
+								changeTab(v) {
+									this.tabValue = v;
+								},
+								closeimgui() {
+									var menu = document.querySelector("#app");
+									menu.style.display = 'none';
+								}
+							}
+						});
+					
