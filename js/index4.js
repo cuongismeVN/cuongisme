ec31af16492b079739092d3b8aba52f3bd4a1e36
cuongismeVN <cskh.cuongisme@gmail.com>
2024-08-26 17:34:36 +0700

    index4.js

--- js/index4.js
+++ js/index4.js
@@ -1,1 +1,9 @@
-có cái lồn nè
+function checkPassword() {
+                var password = document.getElementById('password').value;
+                if (password === "123") { // Thay "your_password" bằng mật khẩu thực tế
+                    document.getElementById('passwordPrompt').style.display = 'none';
+                    document.getElementById('app').style.display = 'block';
+                } else {
+                    alert('Mật khẩu không đúng');
+                }
+            }
