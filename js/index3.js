1ea562ca86ed5e501dca0d898d5e73df993a7e54
cuongismeVN <cskh.cuongisme@gmail.com>
2024-08-26 14:59:29 +0700

    index3.js

--- js/index3.js
+++ js/index3.js
@@ -1,1 +1,130 @@
-có con củ cặc
+// menu by cuong is me
+
+<body> 
+				<div class="container">
+					<div id="titleBar">
+						<div v-show="tabValue==='one'">
+						
+						<div class="container">    
+						
+						</div>
+						
+			<el-collapse-transition>
+							<div v-show="showOption">
+
+				</div>
+
+			</el-collapse-transition>
+			<div style="color: #ffffff;">	<div></div>	
+			
+			<div class="aytext">
+				<div class="aycollapse" @click="showOption = !showOption" style="cursor:pointer; border-radius: 40px; background: rgba(5, 56, 114, 0.9);">
+					<i :class="showOption?'el-icon-caret-bottom':'el-icon-caret-right'"></i>
+					<span >Nhấn Mở Rộng Để Xem</span>
+				</div>
+				
+				<el-collapse-transition>
+					
+					<div v-show="showOption">
+						<div>
+			
+							<div style="color: #ffffff;"> Mỗi Chức Năng Bật Chờ Khoảng 3-5s Cho Nó Load Là Có Tác Dụng. Bấm 1 Lần Rồi Chờ, Không Spam </div> 
+							<br>
+							<hr>
+							 
+						</div>
+					</div>
+			
+			<div>
+			</div>
+			</el-collapse-transition>
+			
+			<div class="color-effect">
+			Bật DNS Trước Khi Bật Các Chức Năng. <br> không Bật Ban 7 Day<div></div>
+			<br>
+			<button style="background-color:  rgba(5, 56, 114, 0.9);height: 47px;width: 67px;border-radius: 10px;"><a href="https://t.me/cheatioscrack" style="text-decoration: none;">Group Telegram</a></button>						
+									</div>
+	
+			</div>
+					</div>			
+					</div>
+					<div v-show="tabValue === 'two'">
+						<div style="color: #ffffff;">BẬT DNS TRƯỚC KHI CHƠI</div>
+			
+						<div>
+			
+							<input onclick="Aim360(this)" name="radio-group1" type="checkbox" id="checkbox1" />
+							<label for="checkbox1" class="checkbox-label" style="margin-right: 60px;">Aim 360</label>
+			
+							<input onclick="s101s(this)" name="radio-group2" type="checkbox" id="checkbox2" />
+							<label for="checkbox2" class="checkbox-label" style="margin-right: 60px;">Quick Gun</label>
+
+			</div>
+			<div>
+			
+						   <input onclick="s105s(this)" name="radio-group3" type="checkbox" id="checkbox3" />
+						   <label for="checkbox3" class="checkbox-label" style="margin-right: 50px;">Aim Awm</label>
+			
+						   <input onclick="s100s(this)" name="radio-group4" type="checkbox" id="checkbox4" />
+						   <label for="checkbox4" class="checkbox-label" style="margin-right: 0px;">Tâm Awm</label>
+			
+			</div>
+			<div>
+			
+						  <input onclick="so01(this)" name="radio-group5" type="checkbox" id="checkbox5" />
+						  <label for="checkbox5" class="checkbox-label" style="margin-right: 32px;">Search Speed</label>
+			
+						  <input onclick="so03(this)" name="radio-group6" type="checkbox" id="checkbox6" />
+						  <label for="checkbox6" class="checkbox-label" style="margin-right: 0px;">Xóa Speed</label>
+	
+						</div>
+						<div class="aytext">
+							<div class="aycollapse" @click="showOption = !showOption" style="cursor:pointer; border-radius: 20px; background: rgba(5, 56, 114, 0.9);">
+								<i :class="showOption?'el-icon-caret-bottom':'el-icon-caret-right'"></i>
+								<span >Nhấn Mở Rộng Để Xem</span>
+							</div>
+							
+							<el-collapse-transition>
+								
+								<div v-show="showOption">
+									<div>
+									
+							<div style="color: #ff0000;"> NHẤN SEARCH SPEED TRƯỚC KHI CHỌN SPEED </div>
+
+							<input onclick="so02(this)" name="radio-group7" type="checkbox" id="checkbox7" />
+							<label for="checkbox7" class="checkbox-label" style="margin-right: 50px;">Speed x10</label>
+			  
+							<input onclick="so05(this)" name="radio-group8" type="checkbox" id="checkbox8" />
+							<label for="checkbox8" class="checkbox-label" style="margin-right: 0px;">Speed x50</label>
+
+						</div>
+						<div>
+
+							<input onclick="so90(this)" name="radio-group9" type="checkbox" id="checkbox9" />
+							<label for="checkbox9" class="checkbox-label" style="margin-right: 60px;">Esp iGG</label>
+			  
+							<input onclick="AimFor(this)" name="radio-group10" type="checkbox" id="checkbox10" />
+							<label for="checkbox10" class="checkbox-label" style="margin-right: 0px;">Aim For</label>
+			
+					
+							</div>
+						</div>
+			
+								<hr>
+							</el-collapse-transition>
+								<div style="color: #ffffff;">	
+								Cuong Is Me<br>Zalo: 0349440360<br>Telegram: cskh_cuongisme
+				
+								</div>
+						</div>
+					</div>
+								<div v-show="tabValue === 'tue'">
+									<div class="aytext">
+				
+								</el-collapse-transition>
+								
+								<div>		
+									<span class="title-age" style="margin-left:25px;font-size: 17px; color: rgb(255, 255, 255);">Tele:@cskh_cuongisme<br><button style="background-color: rgba(5, 56, 114, 0.9);height: 40px;width: 75px;border-radius: 8px;"><a href="https://t.me/cheatmodios/80" style="text-decoration: none;">Cách Dùng Speed</a></button>	
+								
+									<br></pr><button style="background-color: rgba(5, 56, 114, 0.9);height: 43px;width: 65px;border-radius: 8px;"><a href="https://t.me/cskh_cuongisme" style="text-decoration: none;">Telegram Admin</a></button>
+								
