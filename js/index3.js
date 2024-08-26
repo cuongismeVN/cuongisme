f1449962a8cc8d0bef8d170cf151ac53c2c7e683
cuongismeVN <cskh.cuongisme@gmail.com>
2024-08-26 15:10:26 +0700

    index3.js

--- js/index3.js
+++ js/index3.js
@@ -1,19 +1,2 @@
 // by cuong is me
-								<hr>
-							</el-collapse-transition>
-								<div style="color: #ffffff;">	
-								Cuong Is Me<br>Zalo: 0349440360<br>Telegram: cskh_cuongisme
-				
-								</div>
-						</div>
-					</div>
-								<div v-show="tabValue === 'tue'">
-									<div class="aytext">
-				
-								</el-collapse-transition>
-								
-								<div>		
-									<span class="title-age" style="margin-left:25px;font-size: 17px; color: rgb(255, 255, 255);">Tele:@cskh_cuongisme<br><button style="background-color: rgba(5, 56, 114, 0.9);height: 40px;width: 75px;border-radius: 8px;"><a href="https://t.me/cheatmodios/80" style="text-decoration: none;">Cách Dùng Speed</a></button>	
-								
-									<br></pr><button style="background-color: rgba(5, 56, 114, 0.9);height: 43px;width: 65px;border-radius: 8px;"><a href="https://t.me/cskh_cuongisme" style="text-decoration: none;">Telegram Admin</a></button>
-								
+có cái lồn nè
