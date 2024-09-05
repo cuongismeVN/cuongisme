f6d6f19669f1bdee7fc2c97826afa4db90d7e025
cuongismeVN <cskh.cuongisme@gmail.com>
2024-09-05 20:42:45 +0700

    index2.js

--- js/index2.js
+++ js/index2.js
@@ -1,110 +1,72 @@
-// end menu by cuong is me
-var app = new Vue({
-							el: "#app",
-							data() {
-								return {
-									ifshow: true,
-									checked: false,
-									radio: '1',
-									activeSelect: '',
-									selectOption: [{
-
-									}],
-									input: "",
-									showOption: false,
-									tabValue: "one",
-								}
-							},
-							mounted() {
-								this.setRect(360, 320);
-
-								//设置不可拖动
-								setWindowDrag(0, 0, 0, 0);
-
-								var layout = function () {
-									//window.orientation是设备握持方向, 不是屏幕显示方向
-									if (window.lastorientation == window.orientation) return;
-									window.lastorientation = window.orientation;
-
-									//window.screen中的宽高不会随着屏幕旋转更新(只会在初始化的时候固定)
-									if (Math.abs(window.orientation) == 90) {
-										//横屏模式
-										setWindowRect(0, 0, window.screen.height, window.screen.width);
-									} else {
-										//竖屏模式
-										setWindowRect(0, 0, window.screen.width, window.screen.height);
-									}
-								}
-
-								layout(); //设置旋转屏幕时自动调整布局和画布
-								window.addEventListener("orientationchange", layout, false);
-
-								setButtonAction(function () {
-									var menu = document.querySelector("#app");
-									if (menu.style.display == 'none') {
-										menu.style.display = 'block';
-										//显示菜单之后, 设置触控不可穿透悬浮窗口
-										setWindowTouch(true);
-									} else {
-										menu.style.display = 'none';
-										//隐藏菜单之后, 设置触控穿透悬浮窗口
-										setWindowTouch(false);
-									}
-								});
-							},
-
-							methods: {
-								setRect(w, h, x = -1, y = -1) {
-									var boxW = w;
-									var boxH = h;
-
-									var ayMenu = this.$refs.menuMain;
-									ayMenu.style.width = `${boxW}px`;
-									ayMenu.style.height = `${boxH}px`;
-									if (x == -1) ayMenu.style.left = `calc(50% - ${boxW / 2}px)`;
-									if (y == -1) ayMenu.style.top = `calc(50% - ${boxH / 2}px)`;
-
-									ayMenu.style.width = "400px";
-									ayMenu.style.height = "240px";
+// cuong is me
+// xin đừng crack menu em
+// body chức năng menu
 
 
+	
+	<div id="app">
+		<div :class="!ifshow?'onlyTitle ayimgui':'ayimgui'" ref="menuMain">
+			<div class="aytitle">
+			<i :class="ifshow ? 'el-icon-caret-bottom' : 'el-icon-caret-top'" @click.prevent=""></i>
+			
+				<span class="title-age" style="margin-left:18px; color:#f0f0f0;">Project By Cuong Is Me</span><i class="titleClose el-icon el-icon-close" @click="closeimgui"></i>
+			</div>
+			<div class="aybody">
+				<div class="aytab"><span @click="changeTab('one')":style="{background:tabValue==='one'?'#444444':'	#111111'}">Function</span>
+				<span @click="changeTab('two')":style="{background:tabValue==='two'?'#444444':'	#111111'}">Contact</span>
+				</div>
+				<strong>
+				<div v-show="tabValue==='one'"><div class="aytext"><p>Nhấn Contact Để Xem Hướng Dẫn.</p></color>
+					</div>
+                <div class="checkbox-wrapper-24">
+                <div>
 
+					<input type="checkbox" id="autowin" name="check" onclick="so01(this)"><label for="autowin"><span></span>Tìm Speed</label>
 
-									ayMenu.style.borderBottomLeftRadius = "10px";
-									ayMenu.style.borderBottomRightRadius = "10px";
-									ayMenu.style.borderTopLeftRadius = "10px";
-									ayMenu.style.borderTopRightRadius = "10px";
-								},
-								titleTouchStart(event) {
+			    	</div><div>
 
-									this.touchStartX = parseInt(event.touches[0].clientX);
-									this.touchStartY = parseInt(event.touches[0].clientY);
+					<input type="checkbox" id="onehit" name="check" onclick="so06(this)"><label for="onehit"><span></span>Speed x2</label>
 
-									var ayMenu = this.$refs.menuMain;
-									this.menuLastX = ayMenu.offsetLeft;
-									this.menuLastY = ayMenu.offsetTop;
-									document.ayimgui.style.height = '1px';
-									document.container.style.height = '1px';
-									document.aybody.style.height = '1px';
-									document.aytab.style.height = '1px';
+				    </div><div>
+										  
+					<input type="checkbox" id="buffvang" name="check" onclick="so02(this)"><label for="buffvang"><span></span>Speed x10</label>
+					
+				   </div><div>
+											  
+					<input type="checkbox" id="cooldown" name="check" onclick="so05(this)"><label for="cooldown"><span></span>Speed x50</label>
 
-								},
-								titleTouchMove(event) {
-									event.preventDefault();
-									var distanceX = event.touches[0].clientX - this.touchStartX;
-									var distanceY = event.touches[0].clientY - this.touchStartY;
-
-									var ayMenu = this.$refs.menuMain;
-									ayMenu.style.left = this.menuLastX + distanceX + "px";
-									ayMenu.style.top = this.menuLastY + distanceY + "px";
-								},
-								//切换导航栏
-								changeTab(v) {
-									this.tabValue = v;
-								},
-								closeimgui() {
-									var menu = document.querySelector("#app");
-									menu.style.display = 'none';
-								}
-							}
-						});
+				   </div><div>
+				   <input type="checkbox" id="testskin" name="testskin" onclick="so03(this)"><label for="testskin"><span></span>Xóa Speed</label>
+				</div>
+			</div>
+</div>
+			
+					<div v-show="tabValue === 'two'">
+					<div class="aytext">
+						<p>Đọc Kĩ Trước Khi Chơi :</p></div><div style="margin-left: 5px; margin-top:0">
+               
+                  <p class="m1txt">Vào Game Nhấn Tìm Speed Rồi Chọn Speed.</p>
+                  <p class="m1txt">Chơi Xong Trận Tắt Nút Speed.</p>
+				  <p class="m1txt">Sau Đó Nhấn Xóa Speed</p>
+				  <p class="m1txt">Ghép Trận Mới Và Lập Lại</p>
+				  <br>
+                  <b class="m1txt">Contact Me :
+                  </i></b>
+									  <p class="m1txt">Telegram: @cskh_cuongisme</p>
+										  <p class="m1txt">Zalo: 034 944 0360</p>
+										</div> 								
+		</div>
+		</div>
+			<div class="vip">VIP</div>
+		</div>		
+		<!-- Hộp thoại tùy chỉnh -->
+		<div v-if="showCustomPrompt" class="custom-prompt">
+			<div class="prompt-content">
+				<h3>Nhập Key</h3>
+				<input type="text" v-model="inputKey" placeholder="Nhập Key">
+				<div class="button-container">
+					<button @click="confirmKey">Xác Nhận</button>
+					<button @click="copyLink">Get Key </button>
+				</div>
+			</div>
+		</div>
