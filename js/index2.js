dc961b7c1225c16c6314dce5917711d662c76a3a
cuongismeVN <cskh.cuongisme@gmail.com>
2024-09-06 13:06:30 +0700

    index2.js

--- js/index2.js
+++ js/index2.js
@@ -94,7 +94,7 @@
 			},
 			methods: {
 				confirmKey() {
-					const validKey = '';  // thay key menu ở đây
+					const validKey = '777';  // thay key menu ở đây
 					if (this.inputKey === validKey) {
 						this.isKeyValid = true;
 						this.showCustomPrompt = false; // Ẩn hộp thoại khi nhập đúng key
