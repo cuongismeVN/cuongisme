a93c5bd0cb3266dcd714f894448c63183f514822
cuongismeVN <cskh.cuongisme@gmail.com>
2024-09-11 22:30:21 +0700

    index6.js

--- js/index6.js
+++ js/index6.js
@@ -1,1 +1,34 @@
+// body backroud login
+.custom-prompt {
+			position: fixed;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			background-color: rgb(208, 215, 216);
+			padding: 20px;
+			box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+			z-index: 9999;
+		}
 
+		.prompt-content {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+
+		.prompt-content input {
+			margin-bottom: 10px;
+			padding: 5px;
+			width: 200px;
+		}
+
+		.button-container {
+			display: flex;
+			justify-content: space-between;
+			width: 100%;
+		}
+
+		.button-container button {
+			padding: 5px 10px;
+			margin: 0 5px;
+		}
