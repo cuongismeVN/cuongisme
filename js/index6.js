ee29e151742a5d7b3013f1727eefed61db893800
cuongismeVN <cskh.cuongisme@gmail.com>
2024-09-14 17:50:02 +0700

    index6.js

--- js/index6.js
+++ js/index6.js
@@ -7,7 +7,7 @@
 
 const songList = [
     { title: "Em Nào Có Tội", link: "https://thanhdieu.com/files/Em-N%C3%A0o-C%C3%B3-T%E1%BB%99i.mp3" },
-    { title: "Nhạc Tết Chill", link: "https://soundcloud.com/lauraarcher/itsumo-nando-demo-piano" },
+    { title: "Nhạc Tết Chill", link: "https://eoec.ecoe.cc/api/v1/download?sig=Irxsw11gce4FJ04SciZOmWk99m5GMZZm13oaVoGCYKz8L%2BekvduEqVfotxHBk5lMsPkLeEmMQKuaP9KgeUG3oIJwi3feYZcUZKmDvnuX%2F6kxgUEArlS%2BUMNzMceN2BVEpKQ0paJCGsWOIpd1TMTbBLhxi7rKoY6Vn4b5PccUCsNFf%2Fj6tqGxgINXMFaEn3K%2BJfScHRiO%2B0IfddYJBRCPPCBgVcy0MeEw3uwCCZ7l2%2FDlpq4RgwHY5XsGMtOVPwHuBDROytUuU7JrtXJE3wWE9fPdP6ZXV%2FCYfUP2Mq%2BKv1awDRUUYCHSS3XG8AubYaGE04jyBSlw5LPOPeHq%2BHo36g%3D%3D" },
     { title: "Bài hát 3", link: "https://www.example.com/music/song3.mp3" }
 ];
 
