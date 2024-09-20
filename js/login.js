5de1a6971b1acacfb403809db5596db670beee18
cuongismeVN <cskh.cuongisme@gmail.com>
2024-09-20 21:41:33 +0700

    login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,13 @@
-
+<script>
+    loopy()
+      function loopy() {
+      var sWord = ""
+      while (sWord != "1") { // nhập pass ở đây
+      sWord = prompt("Telegram @cskh_cuongisme")
+       } }
+      //  confirm("Nội dung muốn thông báo");
+      //  alert("Nhập Key Thành Công")
+      // window.alert("Hack Free Ai Bán Làm Chó");
+      //   var message = "Đây là cảnh báo!";
+      //  alert(message);
+  </script>
